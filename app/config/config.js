138cbfe12e8bd82bae8ce269408cac69d3fb9fcc
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-08-03 21:26:55 +0200

    Env setup:  - add config package  - add config.js  - expose config.js to our app

---
+++ app/config/config.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const localEnv = _loadLocalEnv(),
+    env = process.env.NODE_ENV || 'development';
+
+
+/**
+ * @property {string} HOST
+ * @property {string} PORT
+ * @property {string} MONGODB_URL
+ */
+const config = {
+
+    development: {
+        HOST: localEnv.HOST || '188.166.63.248',
+        PORT: localEnv.PORT || '9010',
+
+        MONGODB_URL: localEnv.MONGODB_URL || 'mongodb://localhost:27017/differ-canvas'
+    },
+
+    production: {
+        HOST: '127.0.0.1',
+        PORT: '9010',
+
+        MONGODB_URL: 'mongodb://localhost:27017/differ-canvas'
+    }
+};
+
+
+module.exports = config[env];
+
+
+/**
+ * @type function
+ * @returns {{}}
+ * @private
+ * @description Load .env.js file if such exists.
+ */
+function _loadLocalEnv() {
+    let env = {};
+    try {
+        env = require('../../../.env');
+    } catch (e) {
+        if (e instanceof Error && e.code === 'MODULE_NOT_FOUND')
+            console.info('.env is not specified.');
+        else
+            throw e;
+    }
+    return env;
+}
