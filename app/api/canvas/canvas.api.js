e7807f4dbb7ab4327e70eefd44a86e9dd7087d70
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-08-03 23:00:46 +0200

    Canvas route:  - add routes:     - POST /link-account, import.     - GET courses.

--- app/api/canvas/canvas.api.js
+++ app/api/canvas/canvas.api.js
@@ -2,5 +2,23 @@
 
 const router = require('express').Router();
 
+const CanvasSVC = require('./service/canvas.service');
+
+
+router.post('/link-account', (req, res, next) => {
+
+    CanvasSVC.linkAccount(req, res, next);
+});
+
+router.post('/import', (req, res, next) => {
+
+    CanvasSVC.import(req, res, next);
+});
+
+router.get('/courses', (req, res, next) => {
+
+    CanvasSVC.getCourses(req, res, next);
+});
+
 
 module.exports = router;
