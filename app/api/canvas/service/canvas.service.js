68e1c7696304fa927e91a1991a512a50df145468
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-08-03 22:59:02 +0200

    Canvas service:  - add public methods: linkAccount, import, getCourses. Business logic - TBA

--- app/api/canvas/service/canvas.service.js
+++ app/api/canvas/service/canvas.service.js
@@ -1,4 +1,50 @@
 "use strict";
 
 
-module.exports = {};
+module.exports = {
+
+    /**
+     * @type function
+     * @access public
+     * @param req
+     * @param res
+     * @param next
+     * @author Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
+     * @description Link account.
+     */
+    linkAccount: (req, res, next) => {
+        const token = req.body['token'];
+
+        res.json('TBA');
+    },
+
+    /**
+     * @type function
+     * @access public
+     * @param req
+     * @param res
+     * @param next
+     * @author Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
+     * @description Import.
+     */
+    import: (req, res, next) => {
+        const token = req.body['token']; // TODO check for query vs body. Requirement was query.
+
+        res.json('TBA');
+    },
+
+    /**
+     * @type function
+     * @access public
+     * @param req
+     * @param res
+     * @param next
+     * @author Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
+     * @description Get courses.
+     */
+    getCourses: (req, res, next) => {
+        const token = req.query['token'];
+
+        res.json('TBA');
+    }
+};
