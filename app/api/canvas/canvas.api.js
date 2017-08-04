40e4c8a4cc22b4a9253431cf9136dd1e63ed4a96
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-08-04 13:21:55 +0200

    Swagger:  - add annotation for link-account

--- app/api/canvas/canvas.api.js
+++ app/api/canvas/canvas.api.js
@@ -5,8 +5,40 @@ const router = require('express').Router();
 const CanvasSVC = require('./service/canvas.service');
 
 
+/**
+ * @swagger
+ * /link-account:
+ *   post:
+ *     description: Link account.
+ *     operationId: linkAccount
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: linkAccount
+ *         description: LinkAccount object that needs to be added.
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/AddLinkAccountArgs'
+ *     responses:
+ *       201:
+ *         description: Successfully created.
+ *       400:
+ *         description: Invalid input
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ *       409:
+ *         description: Conflict.
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ *       500:
+ *         description: Internal error.
+ *         schema:
+ *           $ref: '#/definitions/ErrorResponse'
+ */
 router.post('/link-account', (req, res, next) => {
-
     CanvasSVC.linkAccount(req, res, next);
 });
 
@@ -22,3 +54,26 @@ router.get('/courses', (req, res, next) => {
 
 
 module.exports = router;
+
+
+/**
+ * @swagger
+ * definitions:
+ *
+ *   AddLinkAccountArgs:
+ *     type: object
+ *     properties:
+ *       token:
+ *         type: string
+ *         example: DP94K9btj2Tj67U9gUDoJc1XgZzgWkIUxgZuyj6wIv3BuaPuLF8dcBcNrmyWCT9z
+ *
+ *   ErrorResponse:
+ *     type: object
+ *     properties:
+ *       errorStatus:
+ *         type: integer
+ *       errorCode:
+ *         type: string
+ *       errorMessage:
+ *         type: string
+ */
