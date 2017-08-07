faa4483b478b8a9fa56a2e2ec40a0418390463d8
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-08-07 10:33:20 +0200

    Test:  - add test for users model

---
+++ test/unit/models/wrapper/users.model.test.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const chai = require('chai'),
+    expect = chai.expect;
+
+const UsersModel = require('../../../../app/models/index').Users;
+
+
+describe('Users Model', () => {
+
+    const userId = 34,
+        courses = [{id: 5, name: 'Data Structures'}];
+
+    const NOT_valid_token = '45fh38rtv0';
+
+    describe('linkAccount', () => {
+        it('should Link Account', (done) => {
+            UsersModel.linkAccount(userId, token, (err, result) => {
+                expect(err).to.be.null;
+
+                expect(result).to.be.instanceof(Object);
+                expect(result).to.have.deep.property('userId', userId);
+                expect(result).to.have.deep.property('token', token);
+                expect(result).to.have.deep.property('courses', []);
+                expect(result).to.have.deep.property('_id');
+                done();
+            });
+        });
+
+        it('should get error. errorCode: ER_DUP_ENTRY.', (done) => {
+            UsersModel.linkAccount(userId, token, (err) => {
+
+                expect(err).to.be.instanceOf(Object);
+                expect(err).to.have.deep.property('code', 11000);
+                expect(err).to.have.deep.property('name', 'MongoError');
+                done();
+            });
+        });
+    });
+
+    describe('getCourses', () => {
+        it('should Get Courses', (done) => {
+            UsersModel.getCourses(token, (err, result) => {
+                expect(err).to.be.null;
+
+                expect(result).to.be.instanceof(Array);
+                expect(result[0]).to.have.deep.property('courses', []);
+                done();
+            });
+        });
+
+        it('should get empty array.', (done) => {
+            UsersModel.getCourses(NOT_valid_token, (err, result) => {
+                expect(err).to.be.null;
+
+                expect(result).to.be.instanceOf(Array).length(0);
+                done();
+            });
+        });
+    });
+
+    describe('saveCourses', () => {
+        it('should Save Courses', (done) => {
+            UsersModel.saveCourses(courses, token, (err, result) => {
+                expect(err).to.be.null;
+
+                expect(result).to.be.instanceof(Object);
+                expect(result).to.have.deep.property('userId', userId);
+                expect(result).to.have.deep.property('token', token);
+                expect(result).to.have.deep.property('_id');
+                expect(result).to.have.deep.property('courses', courses);
+                done();
+            });
+        });
+    });
+});
