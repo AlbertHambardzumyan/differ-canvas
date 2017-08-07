2cdca4c31f884009bd49c8ed3b3c2e9230ebbe3e
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2017-08-07 11:21:40 +0200

    Test:  - update unit test database name.

--- test/unit/models/helper.js
+++ test/unit/models/helper.js
@@ -7,7 +7,7 @@ global.token = 'DP94K9btj2Tj67U9gUDoJc1XgZzgWkIUxgZuyj6wIv3BuaPuLF8dcBcNrmyWCT9z
 
 // database connection init, drop database.
 before((done) => {
-    mongoose.connect('mongodb://localhost/db-test', () => {
+    mongoose.connect('mongodb://localhost/differ-canvas_test_unit', () => {
         mongoose.connection.db.dropDatabase(() => {
             done()
         });
